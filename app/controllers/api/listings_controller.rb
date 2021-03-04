0aacc19e92cf5ccad97bc402dcf9fe453753cddc
brian <riceb53@gmail.com>
2021-03-03 20:32:51 -0500

    allow user to enter address and save lat/lng

--- app/controllers/api/listings_controller.rb
+++ app/controllers/api/listings_controller.rb
@@ -1,5 +1,14 @@
 class Api::ListingsController < ApplicationController
   def create
+    # take the address
+    p params[:address]
+    # put that into geocoder
+    results = Geocoder.search(params[:address])
+    p results.first.coordinates
+    # get the results back
+    # add that to that lat/lng attribute in the model
+    
+    
     @listing = Listing.new(
       description: params[:description],
       year_built: params[:year_built],
@@ -9,6 +18,8 @@ class Api::ListingsController < ApplicationController
       floors: params[:floors],
       availability: params[:availability],
       address: params[:address],
+      latitude: results.first.coordinates[0],
+      longitude: results.first.coordinates[1],
     )
     @listing.save
     render 'show.json.jb'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_04_003649) do
+ActiveRecord::Schema.define(version: 2021_03_04_010506) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 2021_03_04_003649) do
     t.decimal "price", precision: 8, scale: 2
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "latitude"
+    t.float "longitude"
   end
 
 end
