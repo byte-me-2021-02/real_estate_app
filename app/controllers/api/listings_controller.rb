c3532195b3210422c2713547c899cfd229f4f693
brian <riceb53@gmail.com>
2021-03-03 19:51:47 -0500

    add create action for listings, not finding lat/lng yet

---
+++ app/controllers/api/listings_controller.rb
@@ -0,0 +1,16 @@
+class Api::ListingsController < ApplicationController
+  def create
+    @listing = Listing.new(
+      description: params[:description],
+      year_built: params[:year_built],
+      square_feet: params[:square_feet],
+      bedrooms: params[:bedrooms],
+      bathrooms: params[:bathrooms],
+      floors: params[:floors],
+      availability: params[:availability],
+      address: params[:address],
+    )
+    @listing.save
+    render 'show.json.jb'
+  end
+end
