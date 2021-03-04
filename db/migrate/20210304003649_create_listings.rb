4bf52784e536b7a1148936b5f861c9665057074d
brian <riceb53@gmail.com>
2021-03-03 19:37:11 -0500

    init, make listing model

---
+++ db/migrate/20210304003649_create_listings.rb
@@ -0,0 +1,17 @@
+class CreateListings < ActiveRecord::Migration[6.1]
+  def change
+    create_table :listings do |t|
+      t.text :description
+      t.integer :year_built
+      t.integer :square_feet
+      t.decimal :bedrooms, precision: 3, scale: 1
+      t.decimal :bathrooms, precision: 3, scale: 1
+      t.integer :floors
+      t.string :availability
+      t.string :address
+      t.decimal :price, precision: 8, scale: 2
+
+      t.timestamps
+    end
+  end
+end
